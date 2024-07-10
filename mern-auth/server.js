c5837e6bd03424d803b205b6e82993d17852f0a3
bhanu9922 <appalabhanu123@gmail.com>
2024-07-10 15:13:30 +0530

    update server

--- mern-auth/server.js
+++ mern-auth/server.js
@@ -13,7 +13,7 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
